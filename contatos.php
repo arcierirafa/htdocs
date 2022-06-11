bafa9d674d735e262f5927c982dc0daf2b1dcc67
Rafael Arcieri <arcierirafael@gmail.com>
2022-06-10 21:29:25 -0300

    Aula Dia 10-06-22

---
+++ contatos.php
@@ -0,0 +1,14 @@
+<?php
+
+	echo $_POST["nome"]; 
+	echo "<br />";
+	echo $_POST["email"];  
+	echo "<br />";
+	echo $_POST["tipo-contato"];
+	
+	
+	$nome=$_POST["nome"];
+		$email=$_POST["email"];
+			$tipoContato=$_POST["tipo-contato"];
+	
+	
