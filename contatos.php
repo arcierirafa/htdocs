39788ac091fc05e83bc9b151c620d3204766cdbf
arcierirafa <106850888+arcierirafa@users.noreply.github.com>
2022-06-13 20:37:15 -0300

    Aula Finalizada 13/06/22

--- contatos.php
+++ contatos.php
@@ -5,10 +5,34 @@
 	echo $_POST["email"];  
 	echo "<br />";
 	echo $_POST["tipo-contato"];
+	echo "<br />";
+	echo $_POST["devweb"];
+	echo "<br />";
+	echo $_POST["tecnologia1"];
+	echo "<br />";
+	echo $_POST["tecnologia2"];
+	echo "<br />";
+	echo $_POST["tecnologia3"];
+	echo "<br />";
+	echo $_POST["tecnologia4"];
+	echo "<br />";
+	echo $_POST["tecnologia5"];
+	echo "<br />";
+	echo $_POST["tecnologia6"];
+	echo "<br />";
+	echo $_POST["experiencia"];
+	echo "<br />";
 	
-	
-	$nome=$_POST["nome"];
-		$email=$_POST["email"];
-			$tipoContato=$_POST["tipo-contato"];
-	
-	
+	if(isset($_POST["tecnologia1"])){
+		echo $_POST["tecnologia1"];
+	} else{echo "Não Contem";}
+	if(isset?($_POST["tecnologia2"])){
+	} else{echo "Não Contem";}
+	if(isset?($_POST["tecnologia3"])){
+	} else{echo "Não Contem";}
+	if(isset?($_POST["tecnologia4"])){
+	} else{echo "Não Contem";}
+	if(isset?($_POST["tecnologia5"])){
+	} else{echo "Não Contem";}
+	if(isset?($_POST["tecnologia6"])){
+	} else{echo "Não Contem";}
